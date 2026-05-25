427f047b278f55f6dec1188a58a1f63d546e8c6f
sebastian <seba1junio@gmail.com>
2026-05-25 02:05:12 -0300

    feat(04-01): configure CORS headers in Django for frontend origins
    - Add django-cors-headers>=4.0.0 to requirements.txt
    - Register 'corsheaders' in INSTALLED_APPS
    - Add CorsMiddleware as first middleware in MIDDLEWARE
    - Configure CORS_ALLOWED_ORIGINS for dev (5173) and prod (3000)
    - Set CORS_ALLOW_CREDENTIALS = True

--- NodeAlert-Backend/nodealert/settings.py
+++ NodeAlert-Backend/nodealert/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -34,6 +35,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +128,13 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 50,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
+
+# CORS configuration for frontend Docker container
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
